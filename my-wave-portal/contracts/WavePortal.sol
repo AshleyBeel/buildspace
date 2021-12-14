895443a0397e64e4c8077199f86cb5c6b7460ccf
AABeely <ashley.beel@hotmail.co.uk>
2021-12-14 01:05:24 +0000

    add deployment script and individual count mapping

--- my-wave-portal/contracts/WavePortal.sol
+++ my-wave-portal/contracts/WavePortal.sol
@@ -7,13 +7,18 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
+    mapping(address => uint) individualWaveCount;
+
     constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+        console.log("Yo yo, I am a contract and I am smart contract");
     }
 
     function wave() public {
+        address sender = msg.sender;
         totalWaves += 1;
+        individualWaveCount[sender] += 1;
         console.log("%s has waved", msg.sender);
+        console.log("Sender %s has waved %d times", msg.sender, individualWaveCount[msg.sender]);
     }
 
     function getTotalWaves() public view returns (uint256) {
@@ -21,3 +26,6 @@ contract WavePortal {
         return totalWaves;
     }
 }
+
+// Rank wavers based on individual wave counts
+// Display total number of waves
